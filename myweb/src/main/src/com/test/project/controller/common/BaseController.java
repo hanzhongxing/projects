8a9c8b50852b17e3277b89e9e475f8539b075f38
hanzhongxing <hanzhongxing@58ganji.com>
2018-04-08 15:50:46 +0800

    先提交一下

---
+++ myweb/src/main/src/com/test/project/controller/common/BaseController.java
@@ -0,0 +1,16 @@
+package com.test.project.controller.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpRequest;
+
+
+/**
+ * Created by hanzhongxing on 2018/4/8.
+ */
+public class BaseController {
+    protected final static Logger baseLogger= LoggerFactory.getLogger(BaseController.class);
+    protected final static String COMMON_PAGE_ROOT="/common";
+
+
+}
---
+++ myweb/target/myweb/WEB-INF/views/common/noData.jsp
@@ -0,0 +1,11 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hanzhongxing
+  Date: 2018/4/8
+  Time: 14:29
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<json:object name="data">
+    <json:property name="id" value="asdasd"></json:property>
+</json:object>
